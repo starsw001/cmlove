bbabe16856cb67edcd871587da0b855c50fbca15
allen <starsw001@163.com>
2014-12-09 01:17:24 +0800

    小的改动
    小的改动

--- bottomer.php
+++ bottomer.php
@@ -5,7 +5,7 @@
  */
 ?>
 <div class="bottom1">
-    <a href="#top"><img src="/images/bl_top.gif" alt="返回页顶" /></a>
+    <a href="#top"><img src="/images/bl_top.gif" alt="返回顶部" /></a>
 </div>
 <div class="bottom2">
     <a href="/about_us.php">关于我们</a> 　　<a href="/reg.php">免费注册</a> 　　<a href="/login.php">会员登录</a> 　　
@@ -13,9 +13,9 @@
 	<a href="/news.php?kind=2">恋爱宝典</a> 　　<a href="/wrzcnet-terms.php">免责声明</a> 　　<a href="/help">意见反馈</a> 　　
 	<a href="/help">客服中心</a>
     <br/>
-    版权所有<span>&copy;2010~2014</span> 　相约同行网 ( <span class=url>www.baidu.com</span> )
+    版权所有<span>&copy;2010~2014</span> 　相约同行网 ( <span class=url>www.aileyou520.com</span> )
     <br/>
-    客服<span>QQ1：38306293</span> 　客服<span>QQ2：290116505</span> 　电话：<span>020-34506590</span>(正常上班时间)　手机短信：<span>1352 7894 748</span>
+    客服<span>QQ1：807562053</span> 　电话：<span>021-55968315</span>(正常上班时间)　手机：<span>13916671001</span>
     <br/>
     声明：本站属免费交友网站，我们会对信息进行核实，但不承担因此带来的任何责任。查看全文<a href="/wrzcnet-terms.php">免责声明</a>
     <br/>
@@ -32,35 +32,32 @@
                         <tr>
                             <td align="middle" height="22">
                                 <font color="#a5846c">
-                                    地址:<span class="css2">地址</span>&nbsp;&nbsp;&nbsp;联系电话：<span class="css2">电话手机都在这里</span>
+                                    地址:<span class="css2">上海市控江路2063号五环大厦西座1011室</span>&nbsp;&nbsp;&nbsp;联系电话：021-55968315&nbsp;&nbsp;<span class="css2">13916671001</span>
                                 </font>
                             </td>
                         </tr>
                         <tr>
                             <td align="middle" height="22">
                                 <font color="#a5846c">
-                                    <span class="css2">客服QQ:290116505</span>
-                                    <a target="blank" href="tencent://message/?uin=290116505&amp;Site=网软志成客服为你服务&amp;Menu=yes"><img border="0" src="/qqonline.gif" alt="网软志成客服为你服务" /></a>
-									<span class="css2">38306293<a target=blank href=tencent://message/?uin=38306293&Site=网软志成客服为你服务&Menu=yes><img border="0" src=/qqonline.gif alt="网软志成客服为你服务"></a>
-									<span class="css2">417586492</span>
+                                    <span class="css2">客服QQ:807562053</span>
+                                    <a target="blank" href="tencent://message/?uin=807562053&amp;Site=相约同行网客服为你服务&amp;Menu=yes"><img border="0" src="/qqonline.gif" alt="相约同行网客服为你服务" /></a>
+									<span class="css2">807562053</span>
                                     <font face=Verdana>
-                                        <a target=blank href=tencent://message/?uin=417586492&Site=网软志成客服为你服务&Menu=yes><img border="0" src=/qqonline.gif alt="网软志成客服为你服务"></a>
+                                        <a target=blank href=tencent://message/?uin=807562053&Site=相约同行网客服为你服务&Menu=yes><img border="0" src=/qqonline.gif alt="相约同行网客服为你服务"></a>
                                     </font>
-									<span class="css2">657248708</span>
+									<span class="css2">807562053</span>
                                     <font face="Verdana">
-                                        <a target="blank" href="tencent://message/?uin=657248708&amp;Site=网软志成客服为你服务&amp;Menu=yes"><img border="0" src="/qqonline.gif" alt="网软志成客服为你服务" /></a>
+                                        <a target="blank" href="tencent://message/?uin=807562053&amp;Site=相约同行网客服为你服务&amp;Menu=yes"><img border="0" src="/qqonline.gif" alt="相约同行网客服为你服务" /></a>
                                     </font>
                                     <br>
-                                        E-mail:webmaster@wrzc.net MSN:wrtxcn@hotmail.com
-                                    </font>
-                                </font>
+                                        联系我们  E-mail:<a href="mailto:jinlv@shkinglv.com">jinlv@shkinglv.com</a>
+                            	</font>
                             </td>
                         </tr>
                         <tr>
                             <td align="middle" height="22">
                                 <font color="#7f5246">
-                                    Copyright(c) 
-                                    2000-2014 www.wrzc.net <span class="css2">相约同行网</span>版权所有<a href="/admin/login.php" target="_blank" class="topmenu">后台管理登录入口</a>
+                                    2000-2014 www.aileyou520.com <span class="css2">相约同行网</span>版权所有
                                 </font>
                             </td>
                         </tr>
